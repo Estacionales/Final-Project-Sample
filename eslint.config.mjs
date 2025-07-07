d41eb7a49dcee0675c7e9ff792a61c5453324b9c
Estacionales <kebihoya@naver.com>
2025-07-07 10:58:16 +0900

    docs : PR test 파일 생성,

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,17 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    "rules": {
+      "no-var": "off", // var 키워드 사용 가능
+      "prefer-const": "warn", // 변수가 재할당 되지 않는다면 let 대신 const 사용하도록 경고
+      "no-redeclare": 1, // 변수 중복 선언시 경고
+      // 기타 룰 추가
+    },
+  },
+  {
+    ignores: [".history", "dist"],
+  },
 ];
 
 export default eslintConfig;
